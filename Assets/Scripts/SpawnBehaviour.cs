c8802e450a120a5aacc4a9ba6ec04fc00239c37b
Lombard Jérémy <jeremylombard.mail@gmail.com>
2023-09-05 23:34:53 +0200

    {WIP}:Fct_Spawn2
    J'ai rajouté les array pour les game object et le fait que plusieurs peuvent spawn a la fois mais pas encore les contraintes

--- Assets/Scripts/SpawnBehaviour.cs
+++ Assets/Scripts/SpawnBehaviour.cs
@@ -9,11 +9,23 @@ public class SpawnBehaviour : MonoBehaviour
     public float spawnWaitTimeMin;
     public float spawnWaitTimeMax;
 
+    private int randomValuePosition;
+    private int randomValueNumberOfCultist;
+
     public GameObject cultistPrefab;
+
+    List<List<GameObject>> cultistList = new List<List<GameObject>>();
 
     // Start is called before the first frame update
     void Start()
     {
+        //Setup List Of List for stocking GameObject in column
+        cultistList = new List<List<GameObject>>();
+        for (int listNumber = 0; listNumber < 4; listNumber++)
+        {
+            cultistList.Add(new List<GameObject>());
+        }
+
         spawnArray = GetComponentsInChildren<Transform>();
         Debug.Log(spawnArray);
         StartCoroutine(CultistSpawnTimer(spawnWaitTime));
@@ -27,10 +39,23 @@ public class SpawnBehaviour : MonoBehaviour
 
     public void CultistSpawn()
     {
-
-        var cultistPosition = spawnArray[Random.Range(1,5)].transform.position;
-        Debug.Log(cultistPosition);
-        Instantiate(cultistPrefab, cultistPosition, Quaternion.identity);
+        randomValueNumberOfCultist = Random.Range(1, 5);
+        
+        for (int countCultistList = 0; countCultistList < 4; countCultistList++)
+        {
+            foreach(var valueOfItems in cultistList[countCultistList])
+            {
+                Debug.Log(countCultistList,valueOfItems);
+                Debug.Log("==========================");
+            }
+            
+        }
+        for(var counterOfSpawningCultist = 0; counterOfSpawningCultist< randomValueNumberOfCultist; counterOfSpawningCultist++)
+        {
+            randomValuePosition = Random.Range(1, 5);
+            var cultistPosition = spawnArray[randomValuePosition].transform.position;
+            cultistList[randomValuePosition - 1].Add(Instantiate(cultistPrefab, cultistPosition, Quaternion.identity) as GameObject);
+        }
         spawnWaitTime = Random.Range(spawnWaitTimeMin, spawnWaitTimeMax);
         StartCoroutine(CultistSpawnTimer(spawnWaitTime));
     }
