127a7420988f9d7a5299e641729c6538c565c367
Franci-Algera <algera.franci@gmail.com>
2023-09-06 10:52:18 +0200

    Suspicion bar add value

--- Assets/Scripts/ProgressBar/SuspicionBar.cs
+++ Assets/Scripts/ProgressBar/SuspicionBar.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SuspicionBar : MonoBehaviour
 {
     [SerializeField] private GameObjectEvent lostCultist;
-    private float totalValue;
+    private float currentValue;
+    [SerializeField] private float totalValue = 100;
+
+    [SerializeField] private Slider slider;
 
     private void Start()
     {
         lostCultist.scriptableEvent.AddListener(AddSuspicion);
+        if(slider == null) slider = GetComponentInChildren<Slider>();
+        currentValue = 0;
+        slider.value = currentValue;
     }
 
     public void AddSuspicion(object obj)
     {
         float value = (float)obj;
-        totalValue += value;
+        currentValue += value;
+        slider.value = currentValue / totalValue;
         Debug.Log("AddValue : " + value +" : Total value : " + totalValue);
     }
 
